6e364fc3342932f91d9a94d569ec9713f564750e
jovination <jovination791@gmail.com>
2026-04-27 18:04:32 +0300

    few updates

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -11,9 +11,9 @@ export default function Hero(){
             <Button className="bg-[#F3F3F3] text-black hover:bg-[#F3f3f3] ">A movement. A mindset. A mirror of who we are.</Button>
             <h1 className="text-black text-4xl md:text-6xl font-bold text-center">THE AFRICA I KNOW</h1>
             <p className="max-w-md w-full text-center text-[#878787] text-sm">A Pan-African platform celebrating innovation, resilience, and abundance amplifying voices that move our continent forward.</p>
-            <div className="w-full md:w-fit grid grid-cols-1 md:grid-cols-2 gap-3 px-20">
-                <Button className="w-full bg-[#9F8033] h-12 hover:bg-[#9f8033]/90 px-2 md:px-6">Listen the Great Africans     <Mic className="ml-2" /></Button>
-                <Button variant="outline" className="w-full h-12 text-black border-2 border-black/70 px-2 md:px-6"> Follow the Movement <ArrowRight className="ml-2" /></Button>
+            <div className="w-full md:w-fit grid grid-cols-1 md:grid-cols-2 gap-3 px-10">
+                <Button className="w-full bg-[#9F8033] h-12 hover:bg-[#9f8033]/90 px-6">Listen the Great Africans     <Mic className="ml-2" /></Button>
+                <Button variant="outline" className="w-full h-12 text-black border-2 border-black/70 px-6"> Follow the Movement <ArrowRight className="ml-2" /></Button>
             </div>
             <div className="w-full mt-4 grid grid-cols-1 md:grid-cols-2 md:px-15 gap-6">
                 <div className="w-full space-y-5">
