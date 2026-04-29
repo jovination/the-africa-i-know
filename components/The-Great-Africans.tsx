9290af268ac44d787cf933543ad90f1ba23f9095
Jovination <jovination791@gmail.com>
2026-04-29 19:40:06 +0300

    few changes

--- components/The-Great-Africans.tsx
+++ components/The-Great-Africans.tsx
@@ -4,9 +4,16 @@ import { Mic } from "lucide-react";
 
 export default function TheGreatAfricans() {
     return (
-        <div className=" w-full container mx-auto py-12 flex flex-col items-center">
-            <div className="max-w-4xl w-full h-125 bg-[#F5F5F5] rounded-3xl overflow-hidden relative">
-                <Image src="/founder.webp" alt="The Great Africans" fill className="w-full h-full object-cover" />
+        <div className=" w-full container mx-auto py-12 flex flex-col items-center px-6 md:px-8">
+            <div className="max-w-4xl w-full h-125 rounded-2xl overflow-hidden relative">
+                <iframe
+                    src="https://www.youtube.com/embed/ag_I7UOvA8w"
+                    className="w-full h-full"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                    title="The Great Africans"
+                />
             </div>
             <div className="mt-20 flex flex-col items-center">
                      <span className="text-5xl text-center">The Great Africans</span>
@@ -14,7 +21,7 @@ export default function TheGreatAfricans() {
                 <p className="max-w-2xl text-sm text-[#878787] text-center mt-3">The Great Africans is where we sit with them. One conversation. One story. One builder at a time. Each episode brings you into an intimate conversation with an African whose work embodies Ubuntu the belief that our humanity is intertwined, that we do not rise alone. </p>
                 <p className="max-w-2xl text-sm text-[#878787] text-center mt-4">These are leaders, entrepreneurs, healers, advocates, and everyday heroes who are shaping the continent not from a distance, but from within. This is not an interview show. It is a space for truth, reflection, and the kind of honesty that only comes when people speak from the heart. </p>
                 <Button className="bg-[#F3F3F3] text-black hover:bg-[#F3f3f3] mt-4 ">Hosted by Mwamvita Makamba. Guided by Ubuntu.</Button>
-                <div className="flex items-center gap-2 mt-2">
+                <div className="flex items-center gap-2 mt-4">
                     <p>Available on </p>
                     <p className="font-bold"> Apple Podcasts, Spotify, YouTube, and wherever you listen.</p>
                 </div>
