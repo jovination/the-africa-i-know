a65789a321d9f3b2445b52c636a3869f86c2017d
Jovination <jovination791@gmail.com>
2026-05-01 12:47:52 +0300

    few changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter, Abhaya_Libre } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -79,7 +80,9 @@ export default function RootLayout({
       lang="en"
       className={`${inter.variable} ${abhayaLibre.variable} ${kaufmann.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <TooltipProvider>{children}</TooltipProvider>
+      </body>
     </html>
   );
 }
