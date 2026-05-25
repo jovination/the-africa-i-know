1717b4d37748b25044cccf16a5377c1e0a2883be
Jovination <jovination791@gmail.com>
2026-05-26 01:38:32 +0300

    fix images

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,21 @@ const nextConfig: NextConfig = {
     unoptimized: true,
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "lh3.googleusercontent.com",
-        port: "",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'i.ytimg.com'
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.google.com'
+      },
+      {
+        protocol: 'https',
+        hostname: 'drive.usercontent.google.com'
+      }
     ],
   },
   // Production domain configuration
