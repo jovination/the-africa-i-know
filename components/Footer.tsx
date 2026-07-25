8f90dd1b3201940dd9477ab86a20370c6e63ec0f
Jovination <jovination791@gmail.com>
2026-07-25 05:21:37 +0300

    refactor: remove redundant inline styles from Footer images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -14,17 +14,17 @@ export default function Footer(){
              </div>   
             <div className="w-full bg-black px-6 md:px-8">
                 <div className="container mx-auto flex flex-col items-center py-10">
-                    <Image src="/African-new.png" width={150} height={150} alt="logo" style={{ width: 'auto', height: 'auto' }} />
+                    <Image src="/African-new.png" width={150} height={150} alt="logo" />
 
                     <div className="grid grid-cols-3 gap-10 mt-10">
                     <Link href="https://www.instagram.com/theafricaiknow_taik" target="_blank" rel="noopener noreferrer">
-                    <Image src="/instagram.png" width={155} height={155} alt="Instagram" style={{ width: 'auto', height: 'auto' }} />
+                    <Image src="/instagram.png" width={155} height={155} alt="Instagram" />
                     </Link>
                     <Link href="https://open.spotify.com/episode/0GGO5IuSEqxXCi9xDcHMX4" target="_blank" rel="noopener noreferrer">
-                    <Image src="/spotify.png" width={150} height={150} alt="Spotify" style={{ width: 'auto', height: 'auto' }} />
+                    <Image src="/spotify.png" width={150} height={150} alt="Spotify" />
                     </Link>
                     <Link href="https://www.youtube.com/@TheGreatAfricans" target="_blank" rel="noopener noreferrer">
-                    <Image src="/youtube-podcast.png" width={130} height={130}  alt="YouTube" style={{ width: 'auto', height: 'auto' }} />
+                    <Image src="/youtube-podcast.png" width={130} height={130}  alt="YouTube" />
                     </Link>
                     </div>
                     <div className="flex flex-col gap-4 items-center mt-10">
